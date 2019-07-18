8205a9b0a7e782b4cbc2297d59f7e706ec5a2c91
Tomasz Reizer <t_reizer@yahoo.com>
2019-07-18 18:09:30 +0200

    added server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+app.use(express.static('assets'));
+
+app.get('/', function(req,res){
+  res.sendFile('/index.html');
+})
+
+app.get('/userform', function(req,res){
+  const response ={
+    first_name: req.query.first_name,
+    last_name: req.query.last_name
+  };
+});
+
+var server = app.listen(3000,'localhost', function(){
+    var host =server.address().address;
+    var port=server.address().port;
+
+    console.log('Przykøadowa aplikacja nasøuchuje na http://' + host + ':' + port);
+})
+
+app.use(express.static('assets'));
